234f811e5e9b009afc6c316a9ac3281054666191
Thom van Oorschot <Thomvanoorschot@hotmail.com>
2016-10-30 16:17:49 +0100

    Added insurances

--- src/main/java/nl/avans/C3/Domain/Insurance.java
+++ src/main/java/nl/avans/C3/Domain/Insurance.java
@@ -19,10 +19,84 @@ public class Insurance {
     private int sessionsReimbursed;
     private InsuranceCompany insuranceCompany;
     
+    public Insurance(){
+        
+    }
+    
     public Insurance(int ID, String name, BigDecimal price, int sessionsReimbursed){
         this.ID = ID;
         this.name = name;
         this.price = price;
         this.sessionsReimbursed = sessionsReimbursed;
+    }
+
+    /**
+     * @return the ID
+     */
+    public int getID() {
+        return ID;
+    }
+
+    /**
+     * @param ID the ID to set
+     */
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the price
+     */
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    /**
+     * @param price the price to set
+     */
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    /**
+     * @return the sessionsReimbursed
+     */
+    public int getSessionsReimbursed() {
+        return sessionsReimbursed;
+    }
+
+    /**
+     * @param sessionsReimbursed the sessionsReimbursed to set
+     */
+    public void setSessionsReimbursed(int sessionsReimbursed) {
+        this.sessionsReimbursed = sessionsReimbursed;
+    }
+
+    /**
+     * @return the insuranceCompany
+     */
+    public InsuranceCompany getInsuranceCompany() {
+        return insuranceCompany;
+    }
+
+    /**
+     * @param insuranceCompany the insuranceCompany to set
+     */
+    public void setInsuranceCompany(InsuranceCompany insuranceCompany) {
+        this.insuranceCompany = insuranceCompany;
     }
 }
